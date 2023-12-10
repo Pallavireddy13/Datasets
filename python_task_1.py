1eca7ad2961916a871841063f2ac0601da8901b6
Pallavireddy13 <102285289+Pallavireddy13@users.noreply.github.com>
2023-12-10 17:15:59 +0530

    Update python_task_1.py

--- python_task_1.py
+++ python_task_1.py
@@ -1,16 +1,18 @@
-import random
+(ANSWER-1)
+import pandas as pd
 
-def generate_car_matrix(rows, cols):
-    matrix = []
-    for i in range(rows):
-        row = []
-        for j in range(cols):
-            car = random.choice(['Sedan', 'SUV', 'Hatchback'])
-            row.append(car)
-        matrix.append(row)
-    return matrix
+def generate_car_matrix():
+    # Read the dataset as a data frame
+    df = pd.read_csv('dataset-1.csv')
+    
+    # Create a new data frame with id_1 as index and id_2 as columns
+    car_matrix = pd.pivot_table(df, values='car', index='id_1', columns='id_2', fill_value=0)
+    
+    # Set the diagonal values to 0
+    car_matrix.values[[range(car_matrix.shape[0])]*2] = 0
+    
+    return car_matrix
 
-rows = 5
-cols = 3
-car_matrix = generate_car_matrix(rows, cols)
-print(car_matrix)
+
+
+(ANSWER-2)
