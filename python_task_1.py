448e791ad5fc6093f7dbd9e939ba848ba102a9aa
Pallavireddy13 <102285289+Pallavireddy13@users.noreply.github.com>
2023-12-10 17:35:11 +0530

    Update python_task_1.py

--- python_task_1.py
+++ python_task_1.py
@@ -1,4 +1,4 @@
-(ANSWER-1)
+(ANSWER-01)
 import pandas as pd
 
 def generate_car_matrix():
@@ -15,4 +15,34 @@ def generate_car_matrix():
 
 
 
-(ANSWER-2)
+(ANSWER-02)
+import pandas as pd
+
+def get_type_count(df):
+    df['car_type'] = pd.cut(df['car'], bins=[0, 15, 25, float('inf')], labels=['low', 'medium', 'high'])
+    type_count = df['car_type'].value_counts().to_dict()
+    sorted_type_count = dict(sorted(type_count.items()))
+    return sorted_type_count
+
+
+
+
+
+ANSWER-06
+import pandas as pd
+
+def unroll_distance_matrix(df):
+    id_start = []
+    id_end = []
+    distance = []
+
+    for i in range(len(df)):
+        for j in range(len(df)):
+            if df['id'][i] != df['id'][j]:
+                id_start.append(df['id'][i])
+                id_end.append(df['id'][j])
+                distance.append(df['distance'][i])
+
+    unrolled_df = pd.DataFrame({'id_start': id_start, 'id_end': id_end, 'distance': distance})
+    return unrolled_df
+
